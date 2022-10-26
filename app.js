8c02cce4fd7f4bf6ae88358896d8540ec702f7a9
serabu <vpupkin@company.com>
2022-10-26 18:18:46 +0300

    all new

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 function switchClass() {
-  const element = document.querySelector(".row");
+  const element = document.querySelector(".menu__list");
     element.classList.toggle("active");
 }
+const langArr = {
+  "home" :  {
+      "ru": "Главная",
+      "en": "Home ",
+  }, 
+  "products": {
+      "ru": "Продукция",
+      "en": "Products",
+  }, 
+  "about": {
+      "ru": "О нас",
+      "en": "About",
+  }, 
+}
+
+const select = document.querySelector('.change-lang');
+console.log(select)
+const allLang = ['en', 'ru'];
+
+select.addEventListener('change', changeURLLanguage);
+
