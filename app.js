0d245277ef837ae86d9baa35e220502d8e4ba7fd
serabu <ssss9310@gmail.com>
2022-10-27 07:03:27 +0300

    add change language

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
-function switchClass() {
-  const element = document.querySelector(".menu__list");
-    element.classList.toggle("active");
-}
-const langArr = {
-  "home" :  {
-      "ru": "Главная",
-      "en": "Home ",
-  }, 
-  "products": {
-      "ru": "Продукция",
-      "en": "Products",
-  }, 
-  "about": {
-      "ru": "О нас",
-      "en": "About",
-  }, 
-}
 
-const select = document.querySelector('.change-lang');
-console.log(select)
+const select = document.querySelector('select');
 const allLang = ['en', 'ru'];
-
 select.addEventListener('change', changeURLLanguage);
 
+function changeURLLanguage() {
+    let lang = select.value;
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+}
+
+function changeLanguage() {
+    let hash = window.location.hash;
+    hash = hash.substr(1);
+    // console.log(hash);
+    if (!allLang.includes(hash)) {
+        location.href = window.location.pathname + '#ru';
+        location.reload();
+    }
+    select.value = hash;
+    document.querySelector('title').innerHTML = langArr['home'][hash];
+    for (let key in langArr) {
+        let elem = document.querySelector('.lng-' + key);
+        if (elem) {
+            elem.innerHTML = langArr[key][hash];
+        }
+
+    }
+}
+
+changeLanguage();
+
+function switchClass() {
+  const element = document.querySelector(".menu__list");
+    element.classList.toggle("active"); 
+}
