6cfa9bf572b1a45ac182b448281be1406ff309f5
shevcher <shevcher@gene.com>
2022-10-29 13:20:40 +0300

    select change lang

--- app.js
+++ app.js
@@ -4,27 +4,13 @@ ru: "Главная",
 en: "Home ", 
 
 }
-console.log(footerContent)
 
-// const select = document.querySelector('select')
-//     select.addEventListener('change', function(){
-//         let listener = document.querySelector('.header__link-text')
-//         listener.innerHTML = footerContent.en
-//         console.log(select)
-//     })
-        
-    
 const selectElement = document.querySelector('.change-lang');
 
 selectElement.addEventListener('change', (event) => {
   const result = document.querySelector('.header__link-text');
-  result.textContent = ` ${event.target.value}`;
+  result.textContent = footerContent[event.target.value];
 });
-
-
-
-
-
 
 
 function switchClass() {
