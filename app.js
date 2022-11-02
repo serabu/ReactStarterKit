4cf1247c20f1934d2330137b599eefba04e4cc26
serabu <ssss9310@gmail.com>
2022-11-02 06:23:03 +0300

    I will see end?

--- app.js
+++ app.js
@@ -1,19 +1,80 @@
+const pageContent = {
+  Headertext: {
+    home: {
+      ru: "Главная",
+      en: "Home",
+    },
+    products: {
+      ru: "Продукция",
+      en: "Products",
+      children: {
+        germ: {
+          ru: "Немецкие",
+          en: "German",
+        },
+        amer: {
+          ru: "Американские",
+          en: "American",
+        },
+        japan: {
+          ru: "Японские",
+          en: "Japanese",
+        },
+      },
+    },
+    about: {
+      ru: "О нас",
+      en: "About",
+      children: {
+        gall: {
+          ru: "Галерея",
+          en: "Gallery",
+        },
+        cont: {
+          ru: "Контакты",
+          en: "Contacts",
+        },
+      },
+    },
+  },
+  bodytext: {
+    divText: {
+      ru: "Бла бла бла",
+      en: "Bla bla bla",
+    },
+    footertext: {
+      ru: "Футер текст",
+      en: "Footer text",
+    },
+  },
+};
 
-const footerContent = {
-ru: "Главная",
-en: "Home ", 
 
+const selectElement = document.querySelector("select");
+const menu = document.querySelectorAll('.header__link-text, .header__link-submenu');
+
+
+const swapMenuElements = (event) => {
+  const MenuLinks = [...menu]
+  MenuLinks.map(element => {
+    const { change } = element.dataset
+    const Parentalrow = element.closest('.menu__list-item')?.querySelector('.header__link span, .header__link-text').dataset.change
+    const value = pageContent.Headertext[change]
+      ? pageContent.Headertext[change][event.target.value]
+      : pageContent.Headertext[Parentalrow].children[change][event.target.value]
+    element.textContent = value
+  })
 }
+selectElement.addEventListener('change', swapMenuElements, true)
 
-const selectElement = document.querySelector('.change-lang');
 
-selectElement.addEventListener('change', (event) => {
-  const result = document.querySelector('.header__link-text');
-  result.textContent = footerContent[event.target.value];
-});
+
+
+
+
 
 
 function switchClass() {
   const element = document.querySelector(".menu__list");
-    element.classList.toggle("active"); 
+  element.classList.toggle("active");
 }
