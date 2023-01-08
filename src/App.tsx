ce54220f089eb40c21aa5002496b67557b07f864
webgetup <webgetup@gmail.com>
2023-01-09 02:05:43 +0300

    Small refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import * as images from './images/slider';
 function App() {
   return (
     <div className="App">
-      <Slider>
+      <Slider count={3} >
           <img className="slideShow__carPicture" src={images.bmw} aria-label="На фото бемельве " />
           <img className="slideShow__carPicture" src={images.audi} aria-label="На фото бемельве " />
       </Slider>
