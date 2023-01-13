5c51008e73e8355a769fcd6d95cf0506ed28f174
serabu <ssss9310@gmail.com>
2023-01-13 18:46:16 +0300

    do metods

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -15,30 +15,44 @@ const Slider: React.FC<SliderProps> = ({ children, count = 3, dots = false, resp
   const sliderWrapperRef = useRef<HTMLDivElement | null>(null);
   const buttonRight = useRef<HTMLDivElement | null>(null);
   const buttonLeft = useRef<HTMLDivElement | null>(null);
-  const [sliderContainer, setContainer] = useState<HTMLDivElement | null>(null);
+  const [displyBtmLeft, setDisplyBtmLeft ] = useState(false)
+  const [displyBtmRight, setDisplyBtmRight ] = useState(true)
+  const [sliderContainer, setContainer] = useState(0);
   const [width, setWidth] = useState();
   const [showDots, setShowDots] = useState();
   const [dotsCount, setDotsCount] = useState(0);
-  const [widthSlide, setWidthSlide] = useState(0);
+  const [widthSlide, setWidthSlide] = useState(200);
   const [widthOfContainer, setWidthOfContainer] = useState(0);
   const [availableSlidesCount, setAvailableSlidesCount] = useState(count);
   
   const getWidth = (slider: HTMLDivElement) => {
     const container = slider.querySelector('.slideShow__container');
     const wrapper = slider.querySelector('.slideShow__wrapper');
-    setWidthOfContainer((slider.querySelector('.slideShow__slide') as unknown as HTMLElement)?.offsetWidth);
-    const checkCounts = Math.round((slider.offsetWidth - 200) / widthSlide);
-    setWidthOfContainer(widthSlide * checkCounts);
+    setWidthOfContainer((slider.querySelector('.slideShow__slide') as unknown as HTMLElement)?.offsetWidth); 
+    const checkCounts = Math.round((slider.offsetWidth - 200) / widthSlide); // smt wrong here
+    // setWidthOfContainer(widthSlide * availableSlidesCount);
     setAvailableSlidesCount(count < availableSlidesCount ? count : availableSlidesCount);
     setDotsCount(container ? Math.ceil(container.children.length / checkCounts) : 0);
+    
+    
+    
   }
   
   const moveRight = (): void => {
-        // const [width, setWidth] = useState(0)
-        // const onClick = () => setWidth(getSliderWidth(widthSlide?.offsetWidth, count))
-        // container.style.left = setWidth
+    const isHide = availableSlidesCount <= 2
+    setContainer( sliderContainer - widthOfContainer * availableSlidesCount )
+    if (sliderContainer <= 0) setDisplyBtmRight(false)
+    if (isHide) setDisplyBtmRight(true)
   }
-  
+
+  const moveLeft = (): void => {
+
+    setContainer( sliderContainer + widthOfContainer  * availableSlidesCount )
+    if (widthOfContainer + sliderContainer === 0)setDisplyBtmLeft(false);
+    if (sliderContainer < 0) setDisplyBtmLeft(true);
+
+  }
+
   useEffect(() => {
     if (sliderRef.current) {
       getWidth(sliderRef.current);
@@ -51,7 +65,7 @@ const Slider: React.FC<SliderProps> = ({ children, count = 3, dots = false, resp
         <div className="row">
           <div className="slideShow" id="slider-1" role="list">
             <div className="slideShow__wrapper" ref={sliderWrapperRef}  role="listbox">
-              <ul className="slideShow__container" role="listbox">
+              <ul className="slideShow__container" role="listbox" style={{left: sliderContainer}}>
                 {Array.isArray(children)
                   ? children.map((slide, i) => (
                       <li key={`item-${i}`} className="slideShow__slide">{slide}</li>
@@ -64,6 +78,8 @@ const Slider: React.FC<SliderProps> = ({ children, count = 3, dots = false, resp
                 className="slideShow__button slideShow__button--back"
                 aria-label={SliderText.PREVIOUS_SLIDE}
                 role="button"
+                onClick={() => moveLeft()}
+                // style={displyBtmRight? {display: block} : {display: none}}
               >
                 {SliderText.PREVIOUS_SLIDE}
               </button>
