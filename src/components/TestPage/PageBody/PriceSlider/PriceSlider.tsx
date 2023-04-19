9151f7cf412cfa673ce5139b692cfaee0f1edd8c
shevcher <shevcher@gene.com>
2023-04-19 21:44:26 +0300

    TODOs

--- src/components/TestPage/PageBody/PriceSlider/PriceSlider.tsx
+++ src/components/TestPage/PageBody/PriceSlider/PriceSlider.tsx
@@ -5,6 +5,7 @@ export const PriceSlider = () => {
   return (
     <div className="priceSlider">
       <div className="priceSlider__title">
+        {/* TODO Why do you use double underscore after name of element? */}
       <h1 className="priceSlider__title__h1">Featured Properties</h1>
       <h2 className="priceSlider__title__h2">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
--- src/components/TestPage/TestPage.tsx
+++ src/components/TestPage/TestPage.tsx
@@ -3,6 +3,7 @@ import { Header } from "./Header/Header";
 import { PageBody } from "./PageBody/PageBody";
 import "./TestPage.scss";
 
+// TODO: Why do you use this name for page and its component?
 export const TestPage = () => {
   return (
     <div className="testPage">
